50a6693959fa00d780a969462e3e2bcd59b5dd92
BorshuliakM <borshuliakm@gmail.com>
2023-11-18 01:51:48 +0200

    Готово

---
+++ Lab_1/Details.cs
@@ -0,0 +1,19 @@
+﻿public class Details
+{
+    public int Amount { get; set; } = 0;
+    public double Weight { get; set; } = 0;
+    public string Name { get; set; } = "";
+    public string Type { get; set; } = "";
+
+    public Details(string name, string type, int amount, double weight)
+    {
+        Amount = amount;
+        Weight = weight;
+        Name = name;
+        Type = type;
+    }
+    public void Print()
+    {
+        Console.WriteLine($"Name = {Name}, Type = {Type}, Amount = {Amount}, Weight of 1 part (g) = {Weight}");
+    }
+}
--- OOP_Labs/Program.cs
+++ OOP_Labs/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Lab1;
+using System.Text;
+
+Console.OutputEncoding = UTF8Encoding.UTF8;//підключаємо кодування для коректного виведення на консоль
+List<ComponentsInfo> list = new();//створюємо список елементів типу нашого класу
+
+//створюємо екземпляри класу
+ComponentsInfo flange = new ("Фланець", "З", 3, 450);
+ComponentsInfo adapter = new ("Перехідник", "К", 8, 74);
+ComponentsInfo frame = new ("Станина", "Про", 1, 117050);
+
+//додаємо їх до списку
+list.Add(flange);
+list.Add(adapter);
+list.Add(frame);
+
+//за допомогою циклу перебираємо елементи у списку і виводимо їх на консоль методом прінт
+foreach (var item in list) item.Print();
