a4e23ef2e516e7f3d7fc42be1700699b8bfb2096
MaksHalatyr <maksgalatir@gmail.com>
2023-11-22 05:09:58 +0200

    ,

---
+++ Lab9/BisectionMethod.cs
@@ -0,0 +1,37 @@
+﻿namespace Lab9;
+
+// Делегат для представлення функції
+public delegate double EquationDelegate(double x);
+
+public class BisectionMethod
+{
+    // Метод для знаходження кореня рівняння методом поділу проміжку пополам
+    public static double FindRoot(EquationDelegate equation, double a, double b, double epsilon)
+    {
+        if (equation(a) * equation(b) > 0)
+        {
+            throw new ArgumentException("Не виконується умова існування кореня на заданому проміжку.");
+        }
+
+        double result = double.NaN;
+        while ((b - a) / 2 > epsilon)
+        {
+            double c = (a + b) / 2;
+            if (equation(c) == 0.0)
+            {
+                result = c;
+                break;
+            }
+            else if (equation(c) * equation(a) < 0)
+            {
+                b = c;
+            }
+            else
+            {
+                a = c;
+            }
+        }
+
+        return result;
+    }
+}
--- OOP_Labs/Program.cs
+++ OOP_Labs/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Приклад використання
+using Lab9;
+
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+EquationDelegate equation = x => x * x - 4; // Рівняння: x^2 - 4 = 0
+
+double a = 0.0; // Ліва границя проміжку
+double b = 3.0; // Права границя проміжку
+double epsilon = 0.0001; // Точність
+
+try
+{
+    double root = BisectionMethod.FindRoot(equation, a, b, epsilon);
+
+    if (!double.IsNaN(root))
+    {
+        Console.WriteLine($"Корінь рівняння: {root}");
+    }
+    else
+    {
+        Console.WriteLine("На заданому проміжку корінь не знайдено.");
+    }
+}
+catch (ArgumentException ex)
+{
+    Console.WriteLine($"Помилка: {ex.Message}");
+}
