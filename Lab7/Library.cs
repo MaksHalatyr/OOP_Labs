079e74fa9dddaca1e30152e3b84c614a4e26ef28
MaksHalatyr <maksgalatir@gmail.com>
2023-11-22 04:51:21 +0200

    ,

---
+++ Lab7/Library.cs
@@ -0,0 +1,84 @@
+﻿namespace Lab7;
+
+//клас бібліотеки
+public class Library
+{
+    public string Name { get; set; }
+
+    public void GenerateDescription()
+    {
+        Console.WriteLine($"Library: {Name}");
+    }
+}
+
+//клас департаменту
+public class Department
+{
+    public string GenreName { get; set; }
+    public int NumberOfPublications { get; set; }
+
+    public void GenerateDescription()
+    {
+        Console.WriteLine($"Department (Genre): {GenreName}, Number of Publications: {NumberOfPublications}");
+    }
+}
+
+//клас публікації
+public class Publication
+{
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public int PublicationYear { get; set; }
+
+    public void GenerateDescription()
+    {
+        Console.WriteLine($"Publication: {Title}, Author: {Author}, Year: {PublicationYear}");
+    }
+}
+
+//клас книги
+public class Book : Publication
+{
+    public string Summary { get; set; }
+
+    public new void GenerateDescription()
+    {
+        Console.WriteLine($"Book: {Title}, Author: {Author}, Year: {PublicationYear}, Summary: {Summary}");
+    }
+}
+
+//клас журналу
+public class Journal : Publication
+{
+    private List<string> articles = new List<string>();
+
+    public void AddArticle(string article)
+    {
+        articles.Add(article);
+    }
+
+    public new void GenerateDescription()
+    {
+        Console.WriteLine($"Journal: {Title}, Author: {Author}, Year: {PublicationYear}");
+        Console.WriteLine("Articles:");
+        foreach (var article in articles)
+        {
+            Console.WriteLine($"- {article}");
+        }
+    }
+}
+
+//класи винятків
+public class BookException : Exception
+{
+    public BookException(string message) : base(message)
+    {
+    }
+}
+
+public class RikVydannyaException : Exception
+{
+    public RikVydannyaException(string message) : base(message)
+    {
+    }
+}
--- OOP_Labs/Program.cs
+++ OOP_Labs/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Lab7;
+
+Library library = new Library { Name = "City Library" };
+library.GenerateDescription();
+
+// використання класу Department
+Department department = new Department { GenreName = "Fiction", NumberOfPublications = 100 };
+department.GenerateDescription();
+
+// використання класу Publication
+Publication publication = new Publication { Title = "Sample Book", Author = "John Doe", PublicationYear = 2022 };
+publication.GenerateDescription();
+
+// використання класу Book з викликом методу, що може викликати виняток
+Book book = new Book { Title = "C# Programming", Author = "Jane Doe", PublicationYear = 2023, Summary = "" };
+book.GenerateDescription();
+
+// використання класу Journal з додаванням статтей
+Journal journal = new Journal { Title = "Tech Journal", Author = "Tech Writers", PublicationYear = 2023 };
+journal.AddArticle("New Advances in AI");
+journal.AddArticle("Web Development Trends");
+journal.GenerateDescription();
+
+// виклик конструктора класу Видання з викликом винятка RikVydannyaException
+try
+{
+    Publication invalidPublication = new Publication { Title = "Invalid Book", Author = "Invalid Author", PublicationYear = 0 };
+    invalidPublication.GenerateDescription();
+}
+catch (RikVydannyaException ex)
+{
+    Console.WriteLine($"Exception caught: {ex.Message}");
+}
+
+catch (BookException ex)
+{
+    Console.WriteLine($"Book Exception caught: {ex.Message}");
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Exception caught: {ex.Message}");
+}
+
